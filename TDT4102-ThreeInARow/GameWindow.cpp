830724641a6228dd1720d8a7f2dbee6da47028e5
Sindre Brun <sindre.brun@outlook.com>
2019-03-18 16:03:52 +0100

    Remove code for skeleton

--- TDT4102-ThreeInARow/GameWindow.cpp
+++ TDT4102-ThreeInARow/GameWindow.cpp
@@ -7,8 +7,7 @@ const map<Player, Cell> playerToCellState{
 };
 
 GameWindow::GameWindow(Point xy, const string& title):
-	Graph_lib::Window{ xy, width * cellSize, height * cellSize, title },
-	gameOverText{ {Width() / 2, Height() / 2}, ""}
+	Graph_lib::Window{ xy, width * cellSize, height * cellSize, title }
 {
 	// Legg til alle tiles p� vinduet
 	for (int i = 0; i < height; ++i) {
@@ -20,89 +19,12 @@ GameWindow::GameWindow(Point xy, const string& title):
 		}
 	}
 
-	gameOverText.set_color(Color::black);
-	gameOverText.move(-40, 0);
-	gameOverText.set_font_size(20);
-
 	// Fjern window reskalering
 	resizable(nullptr);
 	size_range(x_max(), y_max(), x_max(), y_max());
 }
 
-bool GameWindow::haveNSymbolsInARow(Player p)
-{
-	bool nInARow;
-	// Vi må sjekke alle horisontale og alle vertikale linjer, samt de to diagonalene
-	// Vertikale
-	for (int i{ 0 }; i < width; i++) {
-		nInARow = true;
-		for (int j{ 0 }; j < height; j++) {
-			if (at(j, i).state != playerToCellState.at(p)) {
-				nInARow = false;
-				break;
-			}
-		}
-		if (nInARow) { return true; }
-	}
 
-	// Horisontale
-	for (int j{ 0 }; j < height; j++) {
-		nInARow = true;
-		for (int i{ 0 }; i < width; i++) {
-			if (at(j, i).state != playerToCellState.at(p)) {
-				nInARow = false;
-				break;
-			}
-		}
-		if (nInARow) { return true; }
-	}
-
-	// Diagonal mot høyre
-	nInARow = true;
-	for (int i{ 0 }; i < width; i++) {
-		if (tiles[i * width + i].state != playerToCellState.at(p)) {
-			nInARow = false;
-			break;
-		}
-	}
-	if (nInARow) { return true; }
-
-	//Diagonal mot venstre
-	nInARow = true;
-	for (int i{ 0 }; i < width; i++) {
-		if (at((width - 1 - i), i).state != playerToCellState.at(p)) {
-			nInARow = false;
-			break;
-		}
-	}
-	if (nInARow) { return true; }
-
-	return false;
-}
-
-bool GameWindow::isGameOver()
-{
-	if (gameOver) {
-		return true;
-	}
-
-	if (haveNSymbolsInARow(Player::one)) {
-		gameOver = true;
-		showGameOverText("One won");
-	}
-	if (haveNSymbolsInARow(Player::two)) {
-		gameOver = true;
-		showGameOverText("Two one");
-	}
-	return false;
-}
-
-void GameWindow::showGameOverText(string text)
-{
-	gameOverText.set_label("Two won");
-	attach(gameOverText);
-	flush();
-}
 
 void GameWindow::togglePlayerTurn()
 {
@@ -121,25 +43,7 @@ void GameWindow::cbClick(Address, Address pw)
 		return;
 	}
 
-	// Sjekk om spillet er over
-	if (win.isGameOver()) {
-		return;
-	}
 
-	// Sjekk at ruten ikke har en verdi fra før
-	Tile& clickedTile = win.at(xy);
-	if (clickedTile.state != Cell::empty) {
-		return;
-	}
 
-	// Set symbolet til nåværende spiller i ruten
-	Player currentPlayer = win.getPlayerTurn();
-	Cell playersCell = playerToCellState.at(currentPlayer);
-	clickedTile.set_state(playersCell);
 
-	// Bytt spiller, slik at neste runde setter det andre symbolet
-	win.togglePlayerTurn();
-
-	// Sjekk om noen nå har vunnet
-	win.isGameOver();
 }
